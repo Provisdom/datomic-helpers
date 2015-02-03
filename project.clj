11b06ac382934b2f66f5c978ff25fbcb332b595b
Dave Dixon <dave.dixon@allgress.com>
2015-02-03 14:43:56 -0800

    Update to datascript 0.9.0

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject allgress/datomic-helpers "2.1.3"
+(defproject allgress/datomic-helpers "2.1.4"
   :description "Convenience functions to populate Datomic DB with data, to define DB schema."
   :url "https://github.com/allgress/datomic-helpers"
   :license {:name "MIT License" :url "file://./LICENSE"}
   :dependencies [[org.clojure/clojure "1.7.0-alpha5"]
                  [org.clojure/clojurescript "0.0-2629"]
                  [com.cemerick/clojurescript.test "0.3.1"]
-                 [datascript "0.8.1"]]
+                 [datascript "0.9.0"]]
   :repositories [["s3" {:url "s3p://aurora-repository/releases/"
                         :username :env/aws_key
                         :passphrase :env/aws_secret
